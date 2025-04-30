f6c6d4985597b931d806a07690bf099b5e854be3
Sourav pandit <businessocean.pvt.ltd@gmail.com>
2025-05-01 00:29:02 +0530

    beenable required

--- packages/ui/lib/src/widgets/form/be_enable.dart
+++ packages/ui/lib/src/widgets/form/be_enable.dart
@@ -2,7 +2,7 @@ import 'package:beui/theme.dart';
 import 'package:flutter/widgets.dart';
 
 class BeEnabled extends StatelessWidget {
-  const BeEnabled({super.key, this.enabled = true, required this.child});
+  const BeEnabled({super.key, required this.enabled, required this.child});
 
   final bool enabled;
   final Widget child;
