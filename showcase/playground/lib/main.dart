2b4b72d0e85d1cec55654029768129c231bfebb0
Sourav pandit <businessocean.pvt.ltd@gmail.com>
2025-05-06 22:28:43 +0530

    fix: add more example schema color

--- showcase/playground/lib/main.dart
+++ showcase/playground/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:becomponent/app.dart';
 import 'package:beui/theme.dart';
 import 'package:flutter/material.dart';
-import 'package:playground/use_case_network_service.dart';
+import 'package:playground/use_case_color_schema.dart';
 
 void main() {
   runApp(const AppStateWrapper(child: BegoApp()));
@@ -26,7 +26,7 @@ class BegoApp extends StatelessWidget {
     return MaterialApp(
       themeMode: appState.themeMode,
       theme: BeTheme.buildThemeof(context),
-      home: const BeNetworkServiceDemo(),
+      home: const ColorSchemeDisplay(),
     );
   }
 }
